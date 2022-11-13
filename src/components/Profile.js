cea272c56d2dd3732fff0bb2660a71cb77aa49fe
skandh15 <skandhoff@gmail.com>
2022-11-13 22:20:37 +0530

    Added profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,11 +6,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import '../css/Profile.css';
 
-import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faViber, faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 // Value Import
 import { information, socials } from '../EditMe';
-
+import profile from '../profile.jpg';
 
 function Profile() {
     return (
@@ -22,14 +22,14 @@ function Profile() {
                 </Col>
                 <Col className="column-profile" xs={12} lg={4}>
                     <div className="profile">
-                        <img className="profile-img" src={information.profilePhoto} alt={information.fullName + "'s profile"}></img>
+                        <img className="profile-img" src={profile} alt={information.fullName + "'s profile"}></img>
                     </div>
                     <div className="profile-socials">
-                        <a target="_blank" rel="noreferrer" href={socials.twitterURL} className="profile-twitter">
-                            <FontAwesomeIcon icon = { faTwitter } />
+                        <a target="_blank" rel="noreferrer" href={socials.whatsappURL} className="profile-whatsapp">
+                            <FontAwesomeIcon icon = { faWhatsapp } />
                         </a>
-                        <a target="_blank" rel="noreferrer" href={socials.githubURL} className="profile-github">
-                            <FontAwesomeIcon icon = { faGithub } />
+                        <a target="_blank" rel="noreferrer" href={socials.callURL} className="profile-call">
+                            <FontAwesomeIcon icon = { faViber } />
                         </a>
                         <a target="_blank" rel="noreferrer" href={socials.linkedinURL} className="profile-linkedin">
                             <FontAwesomeIcon icon = { faLinkedin } />
