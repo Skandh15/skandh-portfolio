8932ad453db11ce34f365fe5b1e9fdcfc3932de7
skandh15 <skandhoff@gmail.com>
2022-11-19 20:21:17 +0530

    removed hyperlink from projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -17,7 +17,6 @@ function Portfolio() {
                         <div className="card-info">
                             <Row>
                                 <Col xs={10} className="card-title">{card.projectTitle}</Col>
-                                <Col xs={2} className="card-external-url"><a href={card.projectURL} className="url-icon"><FontAwesomeIcon icon = { faExternalLinkAlt } /></a></Col>
                             </Row>
                             <Row>
                                 <Col className="card-desc">{card.projectDesc}</Col>
