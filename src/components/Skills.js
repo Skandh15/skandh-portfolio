88f501282e0feeca3b4010381929451a494e4b9b
skandh15 <skandhoff@gmail.com>
2022-12-24 16:40:06 +0530

    formatted code for skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PieChart } from 'react-minimal-pie-chart';
-import axios from 'axios';
-
 import '../css/Skills.css';
-
 import { } from '@fortawesome/free-solid-svg-icons'
-
 import { skills } from '../EditMe';
 
 function Skills() {
@@ -17,20 +12,20 @@ function Skills() {
             <Row>
                 <Col xs={12} sm={6}>
                     <PieChart
-                    data={skills.skillsData}
-                    label={({ dataEntry }) => dataEntry.title}
-                    labelStyle={(index) => ({
-                        fill: skills.skillsData[index].color,
-                        fontSize: skills.fontSize,
-                    })}
-                    radius={skills.radius}
-                    labelPosition={skills.labelPosition}
-                    lineWidth={skills.lineWidth}
-                    paddingAngle={skills.paddingAngle}
+                        data={skills.skillsData}
+                        label={({ dataEntry }) => dataEntry.title}
+                        labelStyle={(index) => ({
+                            fill: skills.skillsData[index].color,
+                            fontSize: skills.fontSize,
+                        })}
+                        radius={skills.radius}
+                        labelPosition={skills.labelPosition}
+                        lineWidth={skills.lineWidth}
+                        paddingAngle={skills.paddingAngle}
                     />
                 </Col>
                 <Col xs={12} sm={6} className="skills-panel">
-                    {skills.skillsData.map((skill) => 
+                    {skills.skillsData.map((skill) =>
                         <>
                             <p><span className="skills-highlight">{skill.title}</span><br />- {skill.description}<br />Experience: <span className="skills-highlight">{skill.experience}</span></p>
                         </>
