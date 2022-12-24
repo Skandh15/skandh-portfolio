e39a0b9e00b5ea02365f4df421ff111f173cc775
skandh15 <skandhoff@gmail.com>
2022-12-24 16:41:51 +0530

    formatted code for portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import '../css/Portfolio.css';
-
-import { faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons'
-
 import { portfolioCards } from '../EditMe';
 
 function Portfolio() {
     return (
         <Container className="portfolio-container">
-            <Row style={{justifyContent:"center"}}>
-                {portfolioCards.map((card) => 
+            <Row style={{ justifyContent: "center" }}>
+                {portfolioCards.map((card) =>
                     <Col xs={12} md={6} lg={4} className="portfolio-card">
                         <div className="card-info">
                             <Row>
